0182f536e2bae37131718b673c733c72c35344d8
gauman <78633686+gauman@users.noreply.github.com>
2023-04-16 13:44:03 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,40 @@
+/*
+ * main.c
+ *
+ * Created: 3/17/2023 10:23:57 AM
+ *  Author: 91988
+ */ 
+
+#include <xc.h>
+#define F_CPU 16000000UL 
+#include <util/delay.h>
+#include "I2C_HEADER.h"
+
+
+
+
+
+int main(void)
+{
+
+
+	I2C_init();
+	
+
+	
+	
+    while(1){
+   
+		
+			TWI_start();
+			
+			TWI_write_address(0x20);
+			
+			TWI_write_data(0xff);
+			
+			TWI_stop();
+			
+		    _delay_ms(1000);
+       
+    }
+}
